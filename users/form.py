bb7713373b9bd859e73227e187a26628973c531c
Phuc Le <you@example.com>
2019-11-15 14:17:25 -0800

    User can update information. Blog post now display user's profile picture

--- users/form.py
+++ users/form.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+from .models import Profile
 
 class UserRegisterForm(UserCreationForm):
   # Since the default form does not have email, now we added this 
@@ -18,4 +19,14 @@ class UserRegisterForm(UserCreationForm):
     for fieldname in ['username','email', 'password1', 'password2']:
       self.fields[fieldname].help_text = None
       self.fields[fieldname].error_messages = {'required':'Required'}
-  
+
+class UserUpdateForm(forms.ModelForm):
+  email = forms.EmailField()
+  class Meta:
+    model = User
+    fields = ['username','email']
+
+class ProfileUpdateForm(forms.ModelForm):
+  class Meta:
+    model = Profile
+    fields = ['image']
--- users/views.py
+++ users/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required 
-from .form import UserRegisterForm
+from .form import UserRegisterForm, UserUpdateForm, ProfileUpdateForm
 
 def register(request):
   if request.method == 'POST':
@@ -25,5 +25,22 @@ def register(request):
 
 @login_required # User needs to login before seeing their profile page
 def profile(request):
-  return render(request, 'users/profile.html')
+  if request.method == 'POST':
+    u_form = UserUpdateForm(request.POST, instance=request.user)
+    p_form = ProfileUpdateForm(request.POST,
+    request.FILES,instance=request.user.profile)
+    if u_form.is_valid() and p_form.is_valid:
+      u_form.save()
+      p_form.save()
+      messages.success(request, f'Your account has been updated!')
+      return redirect('profile')
+         
+  else:
+    u_form = UserUpdateForm(instance=request.user)
+    p_form = ProfileUpdateForm()         
 
+  context = {
+    'u_form': u_form,
+    'p_form': p_form,
+  }
+  return render(request, 'users/profile.html', context)

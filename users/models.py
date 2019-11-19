f03ffee172c431e845f589c12054bb1a805c02b8
Phuc Le <you@example.com>
2019-11-18 16:16:15 -0800

    Fix the font size of tittle. Replace default profile pics

--- users/models.py
+++ users/models.py
@@ -3,15 +3,16 @@ from django.contrib.auth.models import User
 from PIL import Image
 class Profile(models.Model):
   user = models.OneToOneField(User, on_delete=models.CASCADE) # Cascade means that if we delete a user, we will delete the blog posted by that user but not otherwise
-  image = models.ImageField(default='default.jpg', upload_to='profile_pics')
+  image = models.ImageField(default='default.jpeg', upload_to='profile_pics')
 
   def __str__(self):
     return f'{self.user.username} Profile'
     
-  def save(self):
-    super().save()
+  def save(self, *args, **kwargs):
+    super(Profile, self).save(*args, **kwargs)
 
     img = Image.open(self.image.path)
+    img.convert("RGB")
 
     if img.height > 300 or img.width > 300:
       output_size = (300, 300)
